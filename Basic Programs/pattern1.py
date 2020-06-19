a8e503ba5bdf06af175bef58a9e78afe859a1d7e
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2020-06-19 11:16:19 +0530

    Update

--- Basic Programs/pattern1.py
+++ Basic Programs/pattern1.py
@@ -1,3 +1,13 @@
+"""
+Program: To print the following pattern.
+        1
+        2 2
+        3 3 3
+        4 4 4 4
+        5 5 5 5 5
+Author:Advaith
+"""
+
 for i in range(1,6):
     for j in range(1,i+1):
         print(i,end=" ")
