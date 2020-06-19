eef7540f9c7257de93949f198ca348a79490b725
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2020-06-19 23:57:17 +0530

    Update

--- Basic Programs/perfect.py
+++ Basic Programs/perfect.py
@@ -1,4 +1,10 @@
+"""
+Program: To check whether a number is perfect or not.A number is said to be perfect
+if the sum of all factors except the number itself is equal to the number.
 
+Eg: 6
+1+2+3=6
+"""
 n=(int)(input("Enter the number "))
 s=0
 for i in range(1,n//2+1):
