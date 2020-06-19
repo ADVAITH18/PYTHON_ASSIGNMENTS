eae08442f935b69c929448c8444f5ffb88c97d13
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2020-06-19 23:40:54 +0530

    Add perfect.py

---
+++ Basic Programs/perfect.py
@@ -0,0 +1,9 @@
+n=(int)input("Enter the number ")
+s=0
+for i in range(1,n//2):
+    if n%i==0:
+        s+=i
+if s==n:
+    print(n," is a perfect number")
+else:
+    print(n," is not a perfect number")
