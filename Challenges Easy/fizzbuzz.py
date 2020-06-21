b923a845c82c5a79f3575930cc066ec361f10e2f
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2020-06-21 12:59:22 +0530

    Add fizzbuzz.py

---
+++ Challenges Easy/fizzbuzz.py
@@ -0,0 +1,10 @@
+"""
+The Fizz Buzz test is a poplular interview question used to help filter out the 99.5% of programming
+ job candidates who can't seem to program their way out of a wet paper bag.
+
+Write a program that returns a list of all the numbers from 1 to an interger argument.
+But for multiples of three use “Fizz” instead of the number and for the multiples of five use “Buzz”.
+For numbers which are multiples of both three and five use “FizzBuzz”.
+
+Eg: fizz_buzz(10) ➞ [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]
+"""
