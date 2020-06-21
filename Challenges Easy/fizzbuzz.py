0bc79c3eaa8061b9bf93351dd48fffcd68fd1a64
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2020-06-21 13:03:50 +0530

    Update

--- Challenges Easy/fizzbuzz.py
+++ Challenges Easy/fizzbuzz.py
@@ -8,3 +8,18 @@ For numbers which are multiples of both three and five use “FizzBuzz”.
 
 Eg: fizz_buzz(10) ➞ [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]
 """
+def fizz_buzz(n):
+    l=[]
+    for i in range(1,n+1):
+        if i%3==0 and i%5==0:
+            l.append('FizzBuzz')
+        elif i%3==0:
+            l.append('Fizz')
+        elif i%5==0:
+            l.append('Buzz')
+        else:
+            l.append(i)
+    return l
+
+n=(int)(input("Enter the number "))
+print(fizz_buzz(n))
